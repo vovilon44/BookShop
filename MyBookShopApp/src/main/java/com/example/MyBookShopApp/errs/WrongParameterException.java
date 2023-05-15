ad3776d22e3a6d323971e419ec944bfb87fa3767
vovilon44 <belial3322@yandex.ru>
2023-05-15 11:43:05 +0300

    first commit

---
+++ MyBookShopApp/src/main/java/com/example/MyBookShopApp/errs/WrongParameterException.java
@@ -0,0 +1,2 @@
+package com.example.MyBookShopApp.errs;public class wrongParameterException {
+}
---
+++ MyBookShopApp/src/test/java/com/example/MyBookShopApp/controllers/BookControllerTest.java
@@ -0,0 +1,47 @@
+package com.example.MyBookShopApp.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BookControllerTest {
+
+    @Test
+    void booksPage() {
+    }
+
+    @Test
+    void saveNewBookImage() {
+    }
+
+    @Test
+    void bookFile() {
+    }
+
+    @Test
+    void botBookFile() {
+    }
+
+    @Test
+    void handleRatingReviewRequest() {
+    }
+
+    @Test
+    void handleReviewMessageRequest() {
+    }
+
+    @Test
+    void handleChangeBookRating() {
+    }
+
+    @Test
+    void handleMy() {
+    }
+
+    @Test
+    void handleMyArchive() {
+    }
+
+    @Test
+    void handleMyHistory() {
+    }
+}
